b549fe13747b988755c0c7ec953311e4c09a2a15
toddmartin <kj4ntv@gmail.com>
2020-12-18 22:45:04 -0800

    Added logic code for game play on the server thread

--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -7,13 +7,59 @@ public class ServerThread extends Thread {
     private Client clientOne;
     private Client clientTwo;
     private Connect4 game;
+    private boolean winState;
 
     public ServerThread (Client c1, Client c2) {
         clientOne = c1;
         clientTwo = c2;
         game = new Connect4(true);
+        winState = false;
     }
 
+    public void setClientOne (Client c) {
+        clientOne = c;
+    }
 
+    public void setClientTwo (Client c) {
+        clientTwo = c;
+    }
+
+    public void start () {
+        clientOne.setPlayerSymbol('x');
+        clientTwo.setPlayerSymbol('o');
+        gameLoop();
+    }
+
+    public void gameLoop () {
+        while (!game.getWinState()) {
+            char playerTurn = game.getPlayerTurn();
+            int move = -1;
+            notifyTurns(playerTurn);
+            while (!game.playerMakesMove(move)) {
+                move = getMove(playerTurn);
+            }
+        }
+        System.out.println("Game is over on this thread");
+
+    }
+
+    private void notifyTurns (char c) {
+        if (clientOne.getPlayerSymbol() == c) {
+            clientOne.writeToClient("1");
+            clientTwo.writeToClient("0");
+        } else if (clientTwo.getPlayerSymbol() == c) {
+            clientOne.writeToClient("0");
+            clientTwo.writeToClient("1");
+        }
+    }
+
+    private int getMove (char c) {
+        if (clientOne.getPlayerSymbol() == c) {
+            return clientOne.receiveMove();
+        } else if (clientTwo.getPlayerSymbol() == c) {
+            return clientTwo.receiveMove();
+        }
+        return -1;
+    }
 
 }
