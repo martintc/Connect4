e1a1572d5c2dce22d58b85cf13b5cf4f5c494dba
toddmartin <kj4ntv@gmail.com>
2020-12-18 21:45:29 -0800

    Initial code for server

--- src/Server/Connect4Server.java
+++ src/Server/Connect4Server.java
@@ -1,4 +1,32 @@
 package Server;
 
+import core.Connect4.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Connect4Server {
+
+    private ServerSocket server;
+    private int port; // designate port to be 8000
+    private ArrayList<Socket> clients;
+
+    public void start () {
+        port = 8000;
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int clientNum = 0;
+
+        while(true) {
+
+        }
+
+    }
+
 }
