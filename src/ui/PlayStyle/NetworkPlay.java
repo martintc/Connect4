62a265cb285ba6730969239778deceb06ccec9e1
toddmartin <kj4ntv@gmail.com>
2020-12-19 23:44:00 -0800

    Added in game states

--- src/ui/PlayStyle/NetworkPlay.java
+++ src/ui/PlayStyle/NetworkPlay.java
@@ -8,7 +8,8 @@ public class NetworkPlay extends PlayStyle {
     private NetworkHandler network;
     private boolean turn;
     private boolean winState;
-    private MessageParser parser;
+    private String boardState;
+    private MessageParser parser = new MessageParser();
 
     public NetworkPlay () {
         network = new NetworkHandler();
@@ -29,6 +30,8 @@ public class NetworkPlay extends PlayStyle {
         parser.parseMessage(message);
         turn = parser.getPlayerTurn();
         winState = parser.getWinState();
+        boardState = parser.getBoardString();
+
     }
 
     public boolean getTurn () {
@@ -37,6 +40,10 @@ public class NetworkPlay extends PlayStyle {
 
     public boolean getWinState () {
         return winState;
+    }
+
+    public String getBoardState () {
+        return boardState;
     }
 
 }
