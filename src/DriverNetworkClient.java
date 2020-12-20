5ee5d8b6f08285bd1df1cd088206689c6c523bac
toddmartin <kj4ntv@gmail.com>
2020-12-19 23:43:43 -0800

    Test Driver for network client

---
+++ src/DriverNetworkClient.java
@@ -0,0 +1,11 @@
+import ui.NetworkUI;
+
+import javafx.application.Application;
+
+public class DriverNetworkClient {
+
+    public static void main (String[] args) {
+        Application.launch(NetworkUI.class, args);
+    }
+
+}
