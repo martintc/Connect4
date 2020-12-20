2236559f6e5fac6cd4b1bdeaf785645769c634d8
toddmartin <kj4ntv@gmail.com>
2020-12-19 23:01:16 -0800

    Changes from an interface to an empty class that will server as a super class of play styles.

--- src/ui/PlayStyle/PlayStyle.java
+++ src/ui/PlayStyle/PlayStyle.java
@@ -1,13 +1,5 @@
 package ui.PlayStyle;
 
-public interface PlayStyle {
-
-    public char getCurrentPlayerSymbol();
-
-    public boolean play (int move);
-
-    public boolean getWinState();
-
-    public String getCurrentBoardString ();
+public class PlayStyle {
 
 }
